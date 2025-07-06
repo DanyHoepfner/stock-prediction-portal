e2ade1a805076c5bb9366ad9bcf04c2082d212a8
Dany Hoepfner <danyhoepfner@yahoo.com>
2025-07-06 08:38:57 +0200

    login functionality

--- frontend-react/src/components/Header.jsx
+++ frontend-react/src/components/Header.jsx
@@ -1,16 +1,42 @@
 import Button from "./Button";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./AuthProvider";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+    setIsLoggedIn(false);
+    navigate("/login");
+  };
+
   return (
     <nav className="navbar container pt-3 pb-3 align-items-start">
       <Link to={"/"} className="text-light navbar-brand">
         Stock Prediction Portal
       </Link>
+
       <div>
-        <Button text="Login" url="/login" classList="btn btn-outline-info" />
-        &nbsp;
-        <Button text="Register" url="/register" classList="btn btn-info" />
+        {isLoggedIn ? (
+          <button className="btn btn-danger" onClick={handleLogout}>
+            Logout
+          </button>
+        ) : (
+          <>
+            <Button
+              text="Login"
+              url="/login"
+              classList="btn btn-outline-info"
+            />
+            &nbsp;
+            <Button text="Register" url="/register" classList="btn btn-info" />
+          </>
+        )}
       </div>
     </nav>
   );
--- frontend-react/src/components/Login.jsx
+++ frontend-react/src/components/Login.jsx
@@ -1,5 +1,97 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./AuthProvider";
+
 const Login = () => {
-  return <div className="text-light">Login</div>;
+  const [userName, setUserName] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [errors, setErrors] = useState({});
+  const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    setLoading(true);
+    e.preventDefault();
+
+    const userData = { username: userName, password: userPassword };
+    console.log("User Data ==> ", userData);
+
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:8000/api/v1/token/",
+        userData
+      );
+      console.log(response);
+      localStorage.setItem("accessToken", response.data.access);
+      localStorage.setItem("refreshToken", response.data.refresh);
+      setIsLoggedIn(true);
+      console.log("Login successful");
+      navigate("/");
+    } catch (error) {
+      console.error("Login error: ", error.response.data);
+      setErrors(error.response.data);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-6 bg-light-dark p-5 rounded-3">
+            <h3 className="text-light text-center mb-4">Login to Portal</h3>
+            <form onSubmit={handleLogin}>
+              <div className="mb-3">
+                <input
+                  type="text"
+                  className="form-control mb-1"
+                  placeholder="Enter username"
+                  value={userName}
+                  onChange={(e) => {
+                    setUserName(e.target.value);
+                  }}
+                />
+              </div>
+
+              <div className="mb-3">
+                <input
+                  type="password"
+                  className="form-control mb-1"
+                  placeholder="Set password"
+                  value={userPassword}
+                  onChange={(e) => {
+                    setUserPassword(e.target.value);
+                  }}
+                />
+              </div>
+
+              {errors && (
+                <div className="text-danger mb-3">{errors.detail}</div>
+              )}
+
+              {loading ? (
+                <button
+                  type="submit"
+                  className="btn btn-info d-block mx-auto"
+                  disabled
+                >
+                  Please wait...
+                </button>
+              ) : (
+                <button type="submit" className="btn btn-info d-block mx-auto">
+                  Login
+                </button>
+              )}
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
